2927bcc8c6e9cbb1fe0e0aad2a1e03bb26701798
Marcelo <marceloalarconbarrenechea@gmail.com>
2024-11-02 16:22:19 -0400

    117 completed

---
+++ migrations/20241102181901_seed_room_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+('General''s Quarters','2024-10-08 14:11:12.236','2024-10-08 14:11:12.236'),
+('Major''s Suite','2024-10-08 14:11:12.236','2024-10-08 14:11:12.236');
---
+++ migrations/20241102182343_seed_restriction_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+('reservation','2024-10-23 15:24:29.495','2024-10-23 15:24:29.495'),
+('owner block','2024-10-23 15:24:29.495','2024-10-23 15:24:29.495');
